343a1d44ba7eda07759b0b15339437792f6fe925
MAHANMALPORU734 <mahan.navidu@gmail.com>
2021-02-20 18:17:17 +0530

    Add folders

---
+++ Instagram/Controllers/CameraViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CameraViewController.swift
+//  Instagram
+//
+//  Created by Mahan Navidu on 2021-02-20.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Instagram/Controllers/ExploreViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ExploreViewController.swift
+//  Instagram
+//
+//  Created by Mahan Navidu on 2021-02-20.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
